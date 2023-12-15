14a8d862db595a4b3daa820a907900ecdf492e61
Anton Zhyvotovskyi <azhyvsoftserve@gmail.com>
2023-12-15 13:36:03 +0200

    WeatherTime component fix

---
+++ src/components/__test__/WeatherTime.test.js
@@ -0,0 +1,253 @@
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import WeatherTime from '../WeatherTime';
+
+const mockStore = configureStore([]);
+
+describe('WeatherTime component', () => {
+  const mockCity = {
+    city: {
+      hourly: [
+        {
+          dt: 1702562400,
+          temp: 7.37,
+          feels_like: 5.31,
+          pressure: 1020,
+          humidity: 89,
+          dew_point: 5.68,
+          uvi: 0.09,
+          clouds: 100,
+          visibility: 10000,
+          wind_speed: 3.06,
+          wind_deg: 226,
+          wind_gust: 9.32,
+          weather: [
+            {
+              id: 804,
+              main: 'Clouds',
+              description: 'overcast clouds',
+              icon: '04d'
+            }
+          ],
+          pop: 0
+        },
+        {
+          dt: 1702566000,
+          temp: 8.45,
+          feels_like: 5.54,
+          pressure: 1020,
+          humidity: 91,
+          dew_point: 6.08,
+          uvi: 0.06,
+          clouds: 100,
+          visibility: 10000,
+          wind_speed: 2.87,
+          wind_deg: 232,
+          wind_gust: 8.8,
+          weather: [
+            {
+              id: 804,
+              main: 'Clouds',
+              description: 'overcast clouds',
+              icon: '04d'
+            }
+          ],
+          pop: 0
+        },
+        {
+          dt: 1702569600,
+          temp: 9.44,
+          feels_like: 6.06,
+          pressure: 1021,
+          humidity: 92,
+          dew_point: 6.23,
+          uvi: 0,
+          clouds: 100,
+          visibility: 10000,
+          wind_speed: 2.17,
+          wind_deg: 247,
+          wind_gust: 7.09,
+          weather: [
+            {
+              id: 804,
+              main: 'Clouds',
+              description: 'overcast clouds',
+              icon: '04n'
+            }
+          ],
+          pop: 0
+        },
+        {
+          dt: 1702573200,
+          temp: 10.43,
+          feels_like: 6.33,
+          pressure: 1022,
+          humidity: 93,
+          dew_point: 6.37,
+          uvi: 0,
+          clouds: 100,
+          visibility: 10000,
+          wind_speed: 1.86,
+          wind_deg: 247,
+          wind_gust: 5.16,
+          weather: [
+            {
+              id: 804,
+              main: 'Clouds',
+              description: 'overcast clouds',
+              icon: '04n'
+            }
+          ],
+          pop: 0
+        },
+        {
+          dt: 1702576800,
+          temp: 11.1,
+          feels_like: 6.07,
+          pressure: 1023,
+          humidity: 95,
+          dew_point: 6.35,
+          uvi: 0,
+          clouds: 96,
+          visibility: 10000,
+          wind_speed: 1.74,
+          wind_deg: 257,
+          wind_gust: 3.29,
+          weather: [
+            {
+              id: 804,
+              main: 'Clouds',
+              description: 'overcast clouds',
+              icon: '04n'
+            }
+          ],
+          pop: 0
+        },
+        {
+          dt: 1702580400,
+          temp: 12.29,
+          feels_like: 5.58,
+          pressure: 1024,
+          humidity: 96,
+          dew_point: 6.1,
+          uvi: 0,
+          clouds: 51,
+          visibility: 10000,
+          wind_speed: 1.76,
+          wind_deg: 265,
+          wind_gust: 2.57,
+          weather: [
+            {
+              id: 803,
+              main: 'Clouds',
+              description: 'broken clouds',
+              icon: '04n'
+            }
+          ],
+          pop: 0
+        }
+      ]
+    }
+  };
+
+  const store = mockStore({ city: mockCity });
+
+  test('renders WeatherTime component with icon', () => {
+    render(
+      <Provider store={store}>
+        <WeatherTime />
+      </Provider>
+    );
+
+    const weatherTimeComponent = screen.getByTestId('weather-time');
+    expect(weatherTimeComponent).toBeInTheDocument();
+  });
+
+  test('renders WeatherTime component with time', () => {
+    render(
+      <Provider store={store}>
+        <WeatherTime />
+      </Provider>
+    );
+
+    const hourElement16 = screen.getByText(/16:00/i);
+    expect(hourElement16).toBeInTheDocument();
+
+    const hourElement17 = screen.getByText(/17:00/i);
+    expect(hourElement17).toBeInTheDocument();
+
+    const hourElement18 = screen.getByText(/18:00/i);
+    expect(hourElement18).toBeInTheDocument();
+
+    const hourElement19 = screen.getByText(/19:00/i);
+    expect(hourElement19).toBeInTheDocument();
+
+    const hourElement20 = screen.getByText(/20:00/i);
+    expect(hourElement20).toBeInTheDocument();
+
+    const hourElement21 = screen.getByText(/21:00/i);
+    expect(hourElement21).toBeInTheDocument();
+  });
+
+  test('renders WeatherTime component with expected icons', () => {
+    render(
+      <Provider store={store}>
+        <WeatherTime />
+      </Provider>
+    );
+
+    const weatherIconElements = screen.getAllByAltText('weatherIcon');
+
+    expect(weatherIconElements[0]).toBeInTheDocument();
+    expect(weatherIconElements[1]).toBeInTheDocument();
+    expect(weatherIconElements[2]).toBeInTheDocument();
+    expect(weatherIconElements[3]).toBeInTheDocument();
+    expect(weatherIconElements[4]).toBeInTheDocument();
+    expect(weatherIconElements[5]).toBeInTheDocument();
+
+    expect(weatherIconElements[0].src).toContain('http://openweathermap.org/img/wn/04d@2x.png');
+    expect(weatherIconElements[0].alt).toContain('weatherIcon');
+
+    expect(weatherIconElements[1].src).toContain('http://openweathermap.org/img/wn/04d@2x.png');
+    expect(weatherIconElements[1].alt).toContain('weatherIcon');
+
+    expect(weatherIconElements[2].src).toContain('http://openweathermap.org/img/wn/04n@2x.png');
+    expect(weatherIconElements[2].alt).toContain('weatherIcon');
+
+    expect(weatherIconElements[3].src).toContain('http://openweathermap.org/img/wn/04n@2x.png');
+    expect(weatherIconElements[3].alt).toContain('weatherIcon');
+
+    expect(weatherIconElements[4].src).toContain('http://openweathermap.org/img/wn/04n@2x.png');
+    expect(weatherIconElements[4].alt).toContain('weatherIcon');
+
+    expect(weatherIconElements[5].src).toContain('http://openweathermap.org/img/wn/04n@2x.png');
+    expect(weatherIconElements[5].alt).toContain('weatherIcon');
+  });
+
+  test('renders WeatherTime component with expected content', () => {
+    render(
+      <Provider store={store}>
+        <WeatherTime />
+      </Provider>
+    );
+
+    const temperatureElement7 = screen.getByText(/7°C/i);
+    expect(temperatureElement7).toBeInTheDocument();
+
+    const temperatureElement8 = screen.getByText(/8°C/i);
+    expect(temperatureElement8).toBeInTheDocument();
+
+    const temperatureElement9 = screen.getByText(/9°C/i);
+    expect(temperatureElement9).toBeInTheDocument();
+
+    const temperatureElement10 = screen.getByText(/10°C/i);
+    expect(temperatureElement10).toBeInTheDocument();
+
+    const temperatureElement11 = screen.getByText(/11°C/i);
+    expect(temperatureElement11).toBeInTheDocument();
+
+    const temperatureElement12 = screen.getByText(/12°C/i);
+    expect(temperatureElement12).toBeInTheDocument();
+  });
+});
